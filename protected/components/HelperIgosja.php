b63b32ca4ff69f5838ac8244e36e364811392b3f
igosja <igosja@ukr.net>
2016-09-04 14:43:21 +0300

    +

---
+++ protected/components/HelperIgosja.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igosja
+ * Date: 04.09.16
+ * Time: 9:17
+ */
---
+++ protected/models/ProductOption.php
@@ -0,0 +1,59 @@
+<?php
+
+class ProductImage extends CActiveRecord
+{
+    public function tableName()
+    {
+        return 'productimage';
+    }
+
+    public function rules()
+    {
+        return array(
+            array('image_id, product_id', 'required'),
+            array('image_id, product_id', 'numerical', 'integerOnly' => true),
+        );
+    }
+
+    public function relations()
+    {
+        return array(
+            'image' => array(self::HAS_ONE, 'Image', array('id' => 'image_id')),
+        );
+    }
+
+    public function attributeLabels()
+    {
+        return array(
+            'id' => 'ID',
+            'category_id' => 'Категория',
+            'name' => 'Название',
+            'price' => 'Цена, грн',
+            'status' => 'Статус',
+            'text' => 'Описание',
+            'url' => 'ЧП Url',
+            'seo_description' => 'SEO description',
+            'seo_keywords' => 'SEO keywords',
+            'seo_title' => 'SEO title',
+        );
+    }
+
+    public function beforeDelete()
+    {
+        if (parent::beforeDelete()) {
+            if (0 < $this->image_id) {
+                $o_image = Image::model()->findByPk($this->image_id);
+                if (file_exists($_SERVER['DOCUMENT_ROOT'] . $o_image->url)) {
+                    unlink($_SERVER['DOCUMENT_ROOT'] . $o_image->url);
+                }
+                $o_image->delete();
+            }
+        }
+        return true;
+    }
+
+    public static function model($className = __CLASS__)
+    {
+        return parent::model($className);
+    }
+}
